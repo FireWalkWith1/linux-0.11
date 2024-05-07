c2da6b216ce6052fc8cd1f7df078d4d4bec6df7c
jiangdewen <jiangdewen@unionwebpay.com>
2024-05-07 14:44:11 +0800

    stage4

--- boot/setup.s
+++ boot/setup.s
@@ -7,6 +7,8 @@ begdata:
 begbss:
 .text
 
+INITSEG=0X9000
+
 entry _start
 _start:
 	mov	ah,#0x03		! read cursor pos
@@ -21,8 +23,30 @@ _start:
 	mov	ax,#0x1301		! write string, move cursor
 	int	0x10
 
-inf_loop:
-	jmp inf_loop
+read_param:
+	mov as,#INITSEG
+	mov ds,ax
+	mov ah,#0x03
+	xor bh,bh
+	int 0x10
+	mov [0],dx
+	mov ah,#0x88
+
+	mov ah,#0x88,
+	int 0x15
+	mov [2],ax
+
+	! 从 0x41 处拷贝 16 个字节（磁盘参数表）
+	mov	ax,#0x0000
+	mov	ds,ax
+	lds si,[4*0x41]
+	mov ax,#INITSEG
+	mov es,ax
+	mov di,#0x0004
+	mov cx,#0x10
+	! 重复16次
+	rep
+	movsb
 	
 msg2:
 	.byte 13,10
