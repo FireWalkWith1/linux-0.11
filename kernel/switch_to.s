c3388561bb3a7e5cc238e52efab77a7c06d76b89
fire <fire.com>
2024-06-03 21:25:54 +0800

    fix

--- kernel/switch_to.s
+++ kernel/switch_to.s
@@ -4,8 +4,6 @@ KERNEL_STACK = 12
 
 .globl switch_to
 
-.align 2
-
 switch_to:
     pushl %ebp
     movl %esp,%ebp
@@ -15,24 +13,24 @@ switch_to:
     movl 8(%ebp),%ebx
     cmpl %ebx,current
     je 1f
-! exchange PCB
+# exchange PCB
     movl %ebx,%eax
     xchgl %eax,current
-! TSS中的内核栈指针的重写
+# TSS中的内核栈指针的重写
     movl tss,%ecx
     addl $4096,%ebx
     movl %ebx,ESP0(%ecx)
-! exchange kernel stack
+# exchange kernel stack
     movl %esp,KERNEL_STACK(%eax)
-! reget ebx
+# reget ebx
     movl 8(%ebp),%ebx
     movl KERNEL_STACK(%ebx),%esp
-! exchange LDT
+# exchange LDT
     movl 12(%ebp),%ecx
     lldt %cx
     movl $0x17,%ecx
     mov %cx,%fs
-! hanle clts etc
+# hanle clts etc
     cmpl %eax,last_task_used_math
     jne 1f
     clts
