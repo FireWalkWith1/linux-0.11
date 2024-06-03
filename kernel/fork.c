39ce69a4e7414632e237d8596013e60b82ed03ab
fire <fire.com>
2024-06-04 01:54:07 +0800

    fix

--- kernel/fork.c
+++ kernel/fork.c
@@ -117,7 +117,7 @@ int copy_process(int nr,long ebp,long edi,long esi,long gs,long none,
 		__asm__("clts ; fnsave %0"::"m" (p->tss.i387));long
 
 	long * krnstack;
-	krnstack = (long)(PAGE_SIZE + (long)p);
+	krnstack = (long *)(PAGE_SIZE + (long)p);
 	*(--krnstack) = ss & 0xffff;
 	*(--krnstack) = esp;
 	*(--krnstack) = eflags;
