0ba9f7ec8e8180c62f8f449b156ce2d9ecd8463a
fire <fire.com>
2024-06-05 22:32:57 +0800

    test

--- lib/pc.c
+++ lib/pc.c
@@ -13,32 +13,27 @@ int main(int argc, char * argv[])
     sem_t * full_sem = sem_open("/full", O_CREAT, 0666, 0);
     int i;
     int isFather = 1;
-    if (isFather) {
-        char c = 0;
-        for (c = 0; c < 500; c++) {
-            printf("wait empty_sem...\n");
-            sem_wait(empty_sem);
-            sem_wait(mutex_sem);
-            printf("write...\n");
-            write(fd, &c, 1);
-            sem_post(mutex_sem);
-            sem_post(full_sem);
-        }
-    }
     for (i = 0; i < 10 && isFather; i++) {
         int wpid = fork();
         if (wpid == 0) {
             printf("fork...\n");
             isFather = 0;
             __pid_t pid = getpid();
-            sem_wait(full_sem);
-            sem_wait(mutex_sem);
-            printf("read...\n");
-            char buffer[1];
-            read(fd, buffer, 1);
-            printf("%d:%c\n", pid, buffer[0]);
-            sem_post(mutex_sem);
-            sem_post(empty_sem);
+            while(1) {
+                int r;
+                sem_wait(full_sem);
+                sem_wait(mutex_sem);
+                printf("read...\n");
+                char buffer[1];
+                read(fd, buffer, 1);
+                printf("%d:%c\n", pid, buffer[0]);
+                sem_post(mutex_sem);
+                sem_post(empty_sem);
+                if (buffer[0] == 499) {
+
+                }
+            }
+            
         }
     }
     if (isFather) {
