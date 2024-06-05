7652210b1a1b5945a8c0be2d71988b82f9fbd46e
fire <fire.com>
2024-06-05 20:05:22 +0800

    fix

--- lib/pc.c
+++ lib/pc.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char * argv[])
 {
-
+    printf("main...\n");
     int fd = open("pc.txt", O_RDWR, 0770);
     sem_t * mutex_sem = sem_open("/mutex", O_CREAT, 0666, 1);
     sem_t * empty_sem = sem_open("/empty", O_CREAT, 0666, 10);
@@ -17,24 +17,24 @@ int main(int argc, char * argv[])
         int wpid = fork();
         if (wpid == 0) {
             isFather = 0;
-            __pid_t pid = getpid;
-            sem_wait(mutex_sem);
+            __pid_t pid = getpid();
             sem_wait(full_sem);
+            sem_wait(mutex_sem);
             char buffer[1];
             read(fd, buffer, 1);
             printf("%d:%c\n", pid, buffer[0]);
-            sem_post(empty_sem);
             sem_post(mutex_sem);
+            sem_post(empty_sem);
         }
     }
     if (isFather) {
         char c = 0;
         for (c = 0; c < 500; c++) {
-            sem_wait(mutex_sem);
             sem_wait(empty_sem);
+            sem_wait(mutex_sem);
             write(fd, &c, 1);
-            sem_post(full_sem);
             sem_post(mutex_sem);
+            sem_post(full_sem);
         }
     }
     
