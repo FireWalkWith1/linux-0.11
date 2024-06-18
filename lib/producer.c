7ce71130d3e49b544e91c7a68e93327057f9000e
fire <fire.com>
2024-06-18 21:07:43 +0800

    printf

--- lib/producer.c
+++ lib/producer.c
@@ -11,6 +11,7 @@ int main(int argc, char const *argv[])
     int * a;
     int i;
     shmid = shmget(111, 1);
+    printf("shmid=%d", shmid);
     a = (int *)shmat(shmid);
     for (i = 0 ; i < 100; i++) {
         *a = i;
